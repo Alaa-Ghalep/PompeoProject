e815e6d30b3ba815c8f267111735f6f28fc18152
Alaa Abu Hamra <114977905+Alaa-Ghalep@users.noreply.github.com>
2022-11-03 18:23:33 +0200

    Add files via upload

---
+++ lista_of_shop.js
@@ -0,0 +1,104 @@
+﻿let list = [{
+    "img": "images/1.png",
+    "name":"Blue Plate",
+    "price":"$ 65.00 USD"
+}, {
+    "img": "images/2.png",
+    "name":"White Potterys",
+    "price":"$ 115.00 USD"
+}, {
+    "img": "images/3.png" ,
+    "name":"Wine Pottery",
+    "price":"$ 95.00 USD"
+}, {
+    "img": "images/4.png" ,
+    "name":"White Cup",
+    "price":"$ 55.00 USD"
+}, {
+    "img": "images/5.png" ,
+    "name": "Red Platte",
+    "price": "$ 115.00 USD"
+}, {
+    "img": "images/6.png" ,
+    "name":"Wine Bowl",
+    "price":"$ 115.00 USD"
+}, {
+    "img": "images/7.png" ,
+    "name":"Gray Cup",
+    "price":"$ 65.00 USD"
+}, {
+    "img": "images/8.png" ,
+    "name":"Decor Plate",
+    "price":"$ 65.00 USD"
+}, {
+    "img": "images/9.png" ,
+    "name":"Mint Pottery",
+    "price":"$ 75.00 USD"
+}, {
+    "img": "images/10.png" ,
+    "name":"Set Of Potterys",
+    "price":"$ 125.00 USD"
+}, {
+    "img": "images/11.png" ,
+    "name":"Orange Ceramic",
+    "price":"$ 55.00 USD"
+}, {
+    "img": "images/12.png" ,
+    "name":"Dark Bowl",
+    "price":"Dark Bowl"
+}, {
+    "img": "images/13.png" ,
+    "name":"Square Pottery",
+    "price":"$ 75.00 USD"
+}, {
+    "img": "images/15.png" ,
+    "name":"Torquers Vaze",
+    "price": "$ 95.00 USD"
+
+}, {
+    "img": "images/16.png" ,
+    "name":" Black Pottery",
+    "price":"$ 115.00 USD"
+}, {
+    "img": "images/17.png" ,
+    "name":"Silver pottery",
+    "price":"$ 100.00 USD"
+}, {
+    "img": "images/18.png",
+    "name":"Mint Bowl",
+    "price":"$ 115.00 USD"
+}, {
+    "img": "images/19.png",
+    "name":"Gold & Black Pottery",
+    "price":"$ 95.00 USD"
+}, {
+    "img": "images/20.png" ,
+    "name":"Orange Plate",
+    "price":"$ 65.00 USD"
+}
+    , {
+    "img": "images/21.png" ,
+        "name": "Red Ceramic",
+        "price": "$ 45.00 USD"
+    },
+    {
+        "img": "images/1.png",
+        "name": "Blue Plate",
+        "price": "$ 65.00 USD"
+    }
+    
+  
+
+
+
+
+
+
+
+
+
+
+
+]
+
+
